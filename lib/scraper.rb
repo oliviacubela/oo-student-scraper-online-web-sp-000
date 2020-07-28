ba6d0c406f4e7095e2db6fcb140adcf126f670db
Olivia Cubela <oliviacubela@gmail.com>
2020-07-28 18:32:51 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,45 @@
+require 'nokogiri'
 require 'open-uri'
-require 'pry'
+# require 'pry'
 
+
+#name: doc.css("div.card-text-container h4.student-name").text
+#location: doc.css("div.card-text-container p.student-location").text
+#profile_url : doc.css(https://learn-co-curriculum.github.io/student-scraper-test-page/students/ryan-johnson.html)
+#profile_url : doc.css(".student-card a").map { |anchor| anchor["href"] }[]
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    doc = Nokogiri::HTML(open(index_url))
+    index_page = doc.css(".student-card a")
+    index_page.collect do |element|
+      {:name => element.css(".student-name").text ,
+        :location => element.css(".student-location").text,
+        :profile_url => element.attr('href')
+      }
+    end
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    students_hash = {}
+
+    profile_page = Nokogiri::HTML(open(profile_url))
+    links = profile_page.css(".social-icon-container").children.css("a").map {|element| element.attribute('href').value}
+    links.each do |link|
+      if link.include?("twitter")
+        students_hash[:twitter] = link
+      elsif link.include?("linkedin")
+        students_hash[:linkedin] = link
+      elsif link.include?("github")
+        students_hash[:github] = link
+      else
+        students_hash[:blog] = link
+      end
+    end
+
+    students_hash[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote").text
+    students_hash[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
+
+    students_hash
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,30 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+    student_hash.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
-    
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
-    
+    self.send("twitter=", attributes_hash[:twitter])
+    self.send("linkedin=", attributes_hash[:linkedin])
+    self.send("github=", attributes_hash[:github])
+    self.send("blog=", attributes_hash[:blog])
+    self.send("profile_quote=", attributes_hash[:profile_quote])
+    self.send("bio=", attributes_hash[:bio])
   end
 
   def self.all
-    
+    @@all
   end
 end
-
